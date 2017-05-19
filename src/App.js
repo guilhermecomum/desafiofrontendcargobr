316299ae4be8b523cb173b7200d5536c62724d34
Guilherme Guerra <guerrinha@comum.org>
2017-05-19 08:39:21 -0300

    Adds members component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Members from './Members'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      members: [],
-      loading: true
-    };
-  }
-
-  componentDidMount() {
-    axios.get('https://api.github.com/orgs/ouishare/members')
-      .then(res => {
-        const members = res.data.map(obj => obj);
-        this.setState({ members, loading: false })
-      });
-  }
-
-  renderLoading() {
-    return (
-      <div>
-        loading...
-      </div>
-    )
-  }
-
-  renderMembers() {
-    const { members } = this.state;
-
-    return (
-      <div>
-      { members.map( member => <div key={member.id}>{member.login}</div>)}
-      </div>
-    )
-  }
-
   render() {
-    const loading = this.state.loading
-
     return (
       <div className="App">
-        <h1>Ouishare's public members</h1>
-        { loading ? this.renderLoading() : this.renderMembers()}
+        <Members />
       </div>
     );
   }
---
+++ src/Members.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Member from './Member';
+
+class Members extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      members: [],
+      loading: true,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/orgs/ouishare/members')
+      .then(res => {
+        const members = res.data.map(obj => obj);
+        this.setState({ members, loading: false })
+      })
+      .catch(error => {
+        this.setState({ loading: false, error: error })
+      })
+  }
+
+  renderError() {
+    return (
+      <div>
+        {this.state.error.message}
+      </div>
+    );
+  }
+
+  renderLoading() {
+    return (
+      <div>
+        loading...
+      </div>
+    )
+  }
+
+  renderMembers() {
+    const { members, error } = this.state;
+
+    if(error) {
+      return this.renderError();
+    }
+
+    return (
+      <div>
+      { members.map( member => <Member key={member.id} member={member} />)}
+      </div>
+    )
+  }
+
+  render() {
+    const loading = this.state.loading
+
+    return (
+      <div className="App">
+        <h1>Ouishare's public members</h1>
+        { loading ? this.renderLoading() : this.renderMembers()}
+      </div>
+    )
+  }
+}
+
+export default Members;
